9280282e8a6817d5a99cec1d58def1346bc3b89d
LiuXinpeng2019102939 <136125471@qq.com>
2020-07-11 18:07:23 +0800

    修改代码

--- layers/modules/__init__.py
+++ layers/modules/__init__.py
@@ -3,5 +3,6 @@ from .multibox_loss import MultiBoxLoss
 from .focal_loss import FocalLoss
 from .GIoU import GiouLoss
 
-# __all__ = ['L2Norm', 'MultiBoxLoss']
+
+# __all__ = ['L2Norm', 'MultiBoxLoss'] L2是正则化
 __all__ = ['L2Norm', 'FocalLoss']
